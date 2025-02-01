8936169569ad26e31b8aa9a7448fd677237477a2
RingLo_ <63995099+lonelyicer@users.noreply.github.com>
2025-02-01 18:52:25 +0800

    feat: theme transition animation

--- .vitepress/theme/locales.ts
+++ .vitepress/theme/locales.ts
@@ -7,7 +7,8 @@ function redirectToLocale() {
         ko: '/Docs/ko/'
     };
 
-    const userLang = navigator.language || navigator.userLanguage;
+    const savedLang = getCookie('savedLang');
+    const userLang = savedLang || navigator.language;
     
     const langPrefix = userLang.toLowerCase().startsWith('zh')
         ? userLang.toLowerCase().includes('cn')
@@ -43,6 +44,24 @@ function redirectToLocale() {
 
     console.log('Redirecting to:', newPath);
     window.location.replace(newPath);
+}
+
+export function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.trim().split('=');
+        if (cookieName === name) {
+            return decodeURIComponent(cookieValue);
+        }
+    }
+    return null;
+}
+
+export function setCookie(name, value, daysToExpire) {
+    const date = new Date();
+    date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${name}=${encodeURIComponent(value)};${expires};path=/`;
 }
 
 export default redirectToLocale;
